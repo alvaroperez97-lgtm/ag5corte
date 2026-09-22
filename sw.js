e77716edf9d3e026bc93ab0c5c100fa188b30d58
alvaroperez97-lgtm <alvaroperez97@gmail.com>
2026-09-22 09:33:45 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -23,7 +23,7 @@
    puerto 8055 del Servidor G5 (LAN), no solo a Tailscale.
    ============================================================ */
 
-const CACHE_VERSION = 'ag5-corte-20260917-103612';
+const CACHE_VERSION = 'ag5-corte-20260918-071153';
 
 const PRECACHE = [
   './',
